b394d58c40d78847fac497f3a264f96c26c31daa
Dmitry <takkand.d@gmail.com>
2019-11-29 13:50:05 +0500

    add tests for tools app

--- tools/tests.py
+++ tools/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 
 # Create your tests here.
+
+
+class SimpleTests(SimpleTestCase):
+    def test_sub_page_status_code(self):
+        response = self.client.get('/tools/sub/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_distance_page_status_code(self):
+        response = self.client.get('/tools/distance/')
+        self.assertEqual(response.status_code, 200)
