39dfbf9de1ec63ed06c66e5d68763d1c0d657e96
Dmitry <takkand.d@gmail.com>
2019-12-23 14:16:36 +0500

    Complete tests, add navbar links

--- db/tests.py
+++ db/tests.py
@@ -1,31 +1,26 @@
 from django.test import TestCase
 from .models import OilField, Well
 
-class DBTests(TestCase):
-    def test_OilField(self):
-        field = OilField.objects.create(
-            name='Pokamas',
-            type='oil',
-            location='surgut',
-            owner='slavneft',
-        )
-        self.assertEqual(field.name, 'Pokamas')
-        self.assertEqual(field.type, 'oil')
-        self.assertEqual(field.owner, 'slavneft')
-        
 
-    def test_Well(self):
-        field = OilField.objects.create(
+class DatabaseTests(TestCase):
+    def setUp(self):
+        self.field = OilField.objects.create(
             name='Pokamas',
             type='oil',
             location='surgut',
             owner='slavneft',
         )
-        well = Well.objects.create(
+        self.well = Well.objects.create(
             name='11W',
-            field=field,
+            field=self.field,
             type='production',
         )
-        self.assertEqual(well.name, '11W')
-        self.assertEqual(well.type, 'production')
-        
+
+    def test_OilField(self):
+        self.assertEqual(self.field.name, 'Pokamas')
+        self.assertEqual(self.field.type, 'oil')
+        self.assertEqual(self.field.owner, 'slavneft')
+
+    def test_Well(self):
+        self.assertEqual(self.well.name, '11W')
+        self.assertEqual(self.well.type, 'production')
--- tools/tests.py
+++ tools/tests.py
@@ -1,13 +1,40 @@
 from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from .views import sub
 
-# Create your tests here.
 
 
-class SimpleTests(SimpleTestCase):
-    def test_sub_page_status_code(self):
-        response = self.client.get('/tools/sub/')
+class StaticPagesTests(SimpleTestCase):
+    def test_sub(self):
+        response = self.client.get(reverse('sub'))
         self.assertEqual(response.status_code, 200)
+        # Check template used
+        self.assertTemplateUsed(response, 'tools/sub.html')
+        # Contains assertion
+        self.assertContains(response, 'Срезки от соседних водозаборов')
+        self.assertNotContains(response, 'Some random text')
+        # resolve - return view function of url
+        view = resolve(reverse('sub'))
+        self.assertEqual(view.func.__name__, sub.__name__)
 
-    def test_distance_page_status_code(self):
-        response = self.client.get('/tools/distance/')
+    def test_distance(self):
+        response = self.client.get(reverse('distance'))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tools/distance.html')
+
+    def test_roxar(self):
+        response = self.client.get(reverse('roxar'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tools/roxar.html')
+
+    def test_water(self):
+        response = self.client.get(reverse('water'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tools/water.html')
+
+    def test_print(self):
+        response = self.client.get(reverse('print'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tools/print.html')
+
+
