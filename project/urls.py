9db013a40bf2acbfda3347fb4765b6eee4936b89
Dmitry <takkand.d@gmail.com>
2020-01-06 19:01:33 +0500

    add profile edit and links

--- project/urls.py
+++ project/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('accounts/', include('allauth.urls')),  # allauth urls
-                  path('', include('pages.urls')),  # Home page
-                  path('db/', include('db.urls')),  # Oil field database
+                  path('accounts/', include('allauth.urls')),  # allauth
+                  path('', include('pages.urls')),  # Home and tools pages
+                  path('db/', include('db.urls')),  # Oil field and well database
                   path('accounts/', include('users.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
