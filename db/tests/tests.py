4eeadb409d24d5fa0e4cc99255c6e9e72d6e6e8f
Dmitry T <takkand.d@gmail.com>
2020-02-18 23:31:09 +0500

    add db test with wells.txt

---
+++ db/tests/tests.py
@@ -0,0 +1,17 @@
+import os
+
+from django.test import TestCase
+from ..models import OilField, Well
+from ..load import upload_wells
+
+
+class DatabaseTests(TestCase):
+    def test_well_load(self):
+        with open('db/tests/wells.txt', 'r') as file:
+            content = file.read()
+        
+        upload_wells(content)
+
+        wells = Well.objects.all()
+        self.assertEqual(len(wells), 5)
+        self.assertEqual(wells[0].name, '12R')
